c359a113a5e031f7a611810ac84f517d988796d6
Kriston Theng <84545628+krhen@users.noreply.github.com>
2021-06-14 07:38:51 -0400

    fix

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -68,7 +68,8 @@ public class Pawn extends ChessPiece {
 		if(!(piece instanceof Pawn)) return false;
 		if(((ChessPiece)piece).isWhite() == this.isWhite()) return false;
 		if(board.pieceMoveCount(piece) != 1) return false;
-		
+		ArrayList<Record> a = board.getRecordings();
+		if(!(a.get(a.size() - 1).equals(board.getSpace(start.getRow(), end.getCol)))) erturn false;
 		return true;
 	}
 	
